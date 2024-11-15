d900d7fca2dc82ff4f49de64bb2ee332230af144
Seif2004t <seif2004t@gmail.com>
2024-11-15 04:48:37 +0200

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
             <h2 className="section-title">Contact</h2>
             <p>Email: <a href="mailto:your.email@example.com" className="text-decoration-none">Seif2004t@gmail.com</a></p>
             <p>Phone: +20 1026019173</p>
-            <p><a href="https://www.linkedin.com/in/seif-tamer-203b7926a" className="text-decoration-none">LinkedIn</a></p>
+            <p><a target="_blank" href="https://www.linkedin.com/in/seif-tamer-203b7926a" className="text-decoration-none">LinkedIn</a></p>
         </section>
     );
 };
